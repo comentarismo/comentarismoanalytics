5a74f165fe36a5ceabefa14a4bd3034f201786ed
thomasmodeneis <thomas.modeneis@gmail.com>
2016-11-13 23:04:16 +0100

    #1

--- express-rate-limit.js
+++ express-rate-limit.js
@@ -4,39 +4,6 @@
 var defaults = require('defaults');
 var url = require("url");
 
-const Queue = require('rethinkdb-job-queue');
-
-var rethinkdbHost = process.env.RETHINKDB_HOST || 'g7-box';
-var rethinkdbPort = process.env.RETHINKDB_PORT || 28015;
-var rethinkdbKey = process.env.RETHINKDB_KEY || '';
-
-var RETHINKDB_QUEUE_TABLE = process.env.RETHINKDB_QUEUE_TABLE || "comentarismosync";
-var targetTimeout = process.env.RETHINKDB_TIMEOUT || 120;
-
-var r = require('rethinkdbdash');
-const connection = r({
-    db: RETHINKDB_QUEUE_TABLE,
-    timeout: targetTimeout,
-
-    servers: [
-        {
-            host: rethinkdbHost,
-            port: rethinkdbPort
-        }
-    ]
-});
-
-const analyticsQueueOptions = {
-    name: "analyticsQueue"
-};
-const analyticsQueue = new Queue(connection, analyticsQueueOptions);
-analyticsQueue.jobOptions = {
-    priority: 'highest',
-    timeout: 300000,
-    retryMax: 5,
-    retryDelay: 60000
-};
-
 function RateLimit(options) {
 
     options = defaults(options, {
@@ -116,26 +83,6 @@ function RateLimit(options) {
             } else {
                 console.log("INFO: Remaining -> ", req.rateLimit.remaining, ip, pathname);
             }
-
-            var v = {
-                app: "comentarismosync",
-                pathname: pathname,
-                headers: req.headers,
-                ip: ip,
-                remaining: parseInt(req.rateLimit.remaining || 0),
-                max: parseInt(options.max || 0),
-                limit: parseInt(limit || 0),
-                time: new Date()
-            };
-
-            // console.log("log view to influxdb, ", v);
-            const job = analyticsQueue.createJob({
-                view: v
-            });
-
-            analyticsQueue.addJob(job).catch((err) => {
-                console.error(err);
-            });
 
             if (options.max && current > options.max) {
                 return options.handler(req, res, next);
