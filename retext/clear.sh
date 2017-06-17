7e882cbea868d31c09246574e98327c9937eba78
Thomas Modeneis <thomas.modeneis@gmail.com>
2017-06-17 22:38:31 +0200

    #5 - Implement rethinkdb password and create related prod cfgs for new k8s rethinkdb cluster

--- retext/clear.sh
+++ retext/clear.sh
@@ -50,3 +50,82 @@ rm -rf retext-spell/.git;
 rm -rf retext-syntax-mentions/.git;
 
 rm -rf retext-quotes/.git;
+
+
+git rm -rf retext/retext --cached
+git rm -rf retext --cached
+
+git rm -rf retext/retext-overuse --cached
+git rm -rf retext-overuse --cached
+
+git rm -rf retext/retext-cliches --cached
+git rm -rf retext-cliches --cached
+
+git rm -rf retext/retext-usage --cached
+git rm -rf retext-usage --cached
+
+git rm -rf retext/retext-simplify --cached
+git rm -rf retext-simplify --cached
+
+git rm -rf retext/retext-equality --cached
+git rm -rf retext-equality --cached
+
+git rm -rf retext/retext-contractions --cached;
+git rm -rf retext-contractions --cached;
+
+git rm -rf retext/retext-diacritics --cached
+git rm -rf retext-diacritics --cached
+
+git rm -rf retext/retext-dutch --cached
+git rm -rf retext-dutch --cached
+
+git rm -rf retext/retext-english --cached
+git rm -rf retext-english --cached
+
+git rm -rf retext/retext-emoji --cached
+git rm -rf retext-emoji --cached
+
+git rm -rf retext/retext-indefinite-article --cached
+git rm -rf retext-indefinite-article --cached
+
+git rm -rf retext/retext-keywords --cached
+git rm -rf retext-keywords --cached
+
+git rm -rf retext/retext-latin --cached
+git rm -rf retext-latin --cached
+
+git rm -rf retext/retext-passive --cached
+git rm -rf retext-passive --cached
+
+git rm -rf retext/retext-profanities --cached
+git rm -rf retext-profanities --cached
+
+git rm -rf retext/retext-readability --cached
+git rm -rf retext-readability --cached
+
+git rm -rf retext/retext-redundant-acronyms --cached
+git rm -rf retext-redundant-acronyms --cached
+
+git rm -rf retext/retext-repeated-words --cached
+git rm -rf retext-repeated-words --cached
+
+git rm -rf retext/retext-sentence-spacing --cached
+git rm -rf retext-sentence-spacing --cached
+
+git rm -rf retext/retext-sentiment --cached
+git rm -rf retext-sentiment --cached
+
+git rm -rf retext/retext-smartypants --cached
+git rm -rf retext-smartypants --cached
+
+git rm -rf retext/retext-spell --cached
+git rm -rf retext-spell --cached
+
+git rm -rf retext/retext-stringify --cached
+git rm -rf retext-stringify --cached
+
+git rm -rf retext/retext-syntax-mentions --cached
+git rm -rf retext-syntax-mentions --cached
+
+git rm -rf retext/retext-quotes --cached
+git rm -rf retext-quotes --cached
