44fb002ab3f455db02a362a21e14303bb7196123
Thomas Modeneis <thomas.modeneis@gmail.com>
2017-06-26 20:53:38 +0200

    #5 - Implement rethinkdb password and create related prod cfgs for new k8s rethinkdb cluster

--- retext/install.sh
+++ retext/install.sh
@@ -3,39 +3,39 @@
 # clone 24 repos
 DIR=$PWD
 
-cd $DIR/retext && npm i && npm run build |true;
-cd $DIR/retext/packages/retext-dutch && npm i && npm run build |true;
-cd $DIR/retext/packages/retext-english && npm i && npm run build |true;
-cd $DIR/retext/packages/retext-latin && npm i && npm run build |true;
-cd $DIR/retext/packages/retext-stringify && npm i && npm run build |true;
+cd $DIR/retext && yarn || true && npm run build;
+cd $DIR/retext/packages/retext-dutch && yarn;
+cd $DIR/retext/packages/retext-english && yarn;
+cd $DIR/retext/packages/retext-latin && yarn;
+cd $DIR/retext/packages/retext-stringify && yarn;
 
-#cd $DIR/retext-dutch && npm i && npm run build;
-#cd $DIR/retext-english && npm i && npm run build;
-#cd $DIR/retext-latin && npm i && npm run build;
-#cd $DIR/retext-stringify && npm i && npm run build;
+#cd $DIR/retext-dutch && yarn;
+#cd $DIR/retext-english && yarn;
+#cd $DIR/retext-latin && yarn;
+#cd $DIR/retext-stringify && yarn;
 
-cd $DIR/retext-overuse && npm i && npm run build |true;
-cd $DIR/retext-cliches && npm i && npm run build |true;
+cd $DIR/retext-overuse && yarn;
+cd $DIR/retext-cliches && yarn;
 
-cd $DIR/retext-usage && npm i && npm run build |true;
+cd $DIR/retext-usage && yarn;
 
-cd $DIR/retext-simplify && npm i && npm run build |true;
-cd $DIR/retext-equality && npm i && npm run build |true;
-cd $DIR/retext-contractions && npm i && npm run build |true;
-cd $DIR/retext-diacritics && npm i && npm run build |true;
+cd $DIR/retext-simplify && yarn;
+cd $DIR/retext-equality && yarn;
+cd $DIR/retext-contractions && yarn;
+cd $DIR/retext-diacritics && yarn;
 
-cd $DIR/retext-emoji && npm i && npm run build |true;
-cd $DIR/retext-indefinite-article && npm i && npm run build |true;
-cd $DIR/retext-keywords && npm i && npm run build |true;
+cd $DIR/retext-emoji && yarn;
+cd $DIR/retext-indefinite-article && yarn;
+cd $DIR/retext-keywords && yarn;
 
-cd $DIR/retext-passive && npm i && npm run build |true;
-cd $DIR/retext-profanities && npm i && npm run build |true;
-cd $DIR/retext-readability && npm i && npm run build |true;
-cd $DIR/retext-redundant-acronyms && npm i && npm run build |true;
-cd $DIR/retext-repeated-words && npm i && npm run build |true;
-cd $DIR/retext-sentence-spacing && npm i && npm run build |true;
-cd $DIR/retext-sentiment && npm i && npm run build |true;
-cd $DIR/retext-smartypants && npm i && npm run build |true;
-cd $DIR/retext-spell && npm i && npm run build |true;
-cd $DIR/retext-syntax-mentions && npm i && npm run build |true;
-cd $DIR/retext-quotes && npm i && npm run build |true;
+cd $DIR/retext-passive && yarn;
+cd $DIR/retext-profanities && yarn;
+cd $DIR/retext-readability && yarn;
+cd $DIR/retext-redundant-acronyms && yarn;
+cd $DIR/retext-repeated-words && yarn;
+cd $DIR/retext-sentence-spacing && yarn;
+cd $DIR/retext-sentiment && yarn;
+cd $DIR/retext-smartypants && yarn;
+cd $DIR/retext-spell && yarn;
+cd $DIR/retext-syntax-mentions && yarn;
+cd $DIR/retext-quotes && yarn;
