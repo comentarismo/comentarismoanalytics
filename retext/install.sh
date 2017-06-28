eace056f5639c4fb64752d33b07a7bdc16494dd0
Thomas Modeneis <thomas.modeneis@gmail.com>
2017-06-28 19:57:48 +0200

    fixing build

--- retext/install.sh
+++ retext/install.sh
@@ -3,39 +3,39 @@
 # clone 24 repos
 DIR=$PWD/retext
 
-cd $DIR/retext && yarn || true && npm run build;
-cd $DIR/retext/packages/retext-dutch && yarn ||true && npm run build || true;
-cd $DIR/retext/packages/retext-english && yarn ||true && npm run build || true;
-cd $DIR/retext/packages/retext-latin && yarn ||true && npm run build || true;
-cd $DIR/retext/packages/retext-stringify && yarn ||true && npm run build || true;
+cd $DIR/retext && rm -rf .git ||true && rm -rf node_modules ||true && yarn || true && npm run build;
+cd $DIR/retext/packages/retext-dutch && rm -rf .git ||true && rm -rf node_modules ||true && yarn  ||true && npm run build || true;
+cd $DIR/retext/packages/retext-english && rm -rf .git ||true && rm -rf node_modules ||true && yarn  ||true && npm run build || true;
+cd $DIR/retext/packages/retext-latin && rm -rf .git ||true && rm -rf node_modules ||true && yarn  ||true && npm run build || true;
+cd $DIR/retext/packages/retext-stringify && rm -rf .git ||true && rm -rf node_modules ||true && yarn  ||true && npm run build || true;
 
-#cd $DIR/retext-dutch && yarn;
-#cd $DIR/retext-english && yarn;
-#cd $DIR/retext-latin && yarn;
-#cd $DIR/retext-stringify && yarn;
+#cd $DIR/retext-dutch && rm -rf .git ||true && rm -rf node_modules ||true && yarn ;
+#cd $DIR/retext-english && rm -rf .git ||true && rm -rf node_modules ||true && yarn ;
+#cd $DIR/retext-latin && rm -rf .git ||true && rm -rf node_modules ||true && yarn ;
+#cd $DIR/retext-stringify && rm -rf .git ||true && rm -rf node_modules ||true && yarn ;
 
-cd $DIR/retext-overuse && yarn ||true && npm run build || true;
-cd $DIR/retext-cliches && yarn ||true && npm run build || true;
+cd $DIR/retext-overuse && rm -rf .git ||true && rm -rf node_modules ||true && yarn  ||true && npm run build || true;
+cd $DIR/retext-cliches && rm -rf .git ||true && rm -rf node_modules ||true && yarn  ||true && npm run build || true;
 
-cd $DIR/retext-usage && yarn ||true && npm run build || true;
+cd $DIR/retext-usage && rm -rf .git ||true && rm -rf node_modules ||true && yarn  ||true && npm run build || true;
 
-cd $DIR/retext-simplify && yarn ||true && npm run build || true;
-cd $DIR/retext-equality && yarn ||true && npm run build || true;
-cd $DIR/retext-contractions && yarn ||true && npm run build || true;
-cd $DIR/retext-diacritics && yarn ||true && npm run build || true;
+cd $DIR/retext-simplify && rm -rf .git ||true && rm -rf node_modules ||true && yarn  ||true && npm run build || true;
+cd $DIR/retext-equality && rm -rf .git ||true && rm -rf node_modules ||true && yarn  ||true && npm run build || true;
+cd $DIR/retext-contractions && rm -rf .git ||true && rm -rf node_modules ||true && yarn  ||true && npm run build || true;
+cd $DIR/retext-diacritics && rm -rf .git ||true && rm -rf node_modules ||true && yarn  ||true && npm run build || true;
 
-cd $DIR/retext-emoji && yarn ||true && npm run build || true;
-cd $DIR/retext-indefinite-article && yarn ||true && npm run build || true;
-cd $DIR/retext-keywords && yarn ||true && npm run build || true;
+cd $DIR/retext-emoji && rm -rf .git ||true && rm -rf node_modules ||true && yarn  ||true && npm run build || true;
+cd $DIR/retext-indefinite-article && rm -rf .git ||true && rm -rf node_modules ||true && yarn  ||true && npm run build || true;
+cd $DIR/retext-keywords && rm -rf .git ||true && rm -rf node_modules ||true && yarn  ||true && npm run build || true;
 
-cd $DIR/retext-passive && yarn ||true && npm run build || true;
-cd $DIR/retext-profanities && yarn ||true && npm run build || true;
-cd $DIR/retext-readability && yarn ||true && npm run build || true;
-cd $DIR/retext-redundant-acronyms && yarn ||true && npm run build || true;
-cd $DIR/retext-repeated-words && yarn ||true && npm run build || true;
-cd $DIR/retext-sentence-spacing && yarn ||true && npm run build || true;
-cd $DIR/retext-sentiment && yarn || true && npm run build || true;
-cd $DIR/retext-smartypants && yarn ||true && npm run build || true;
-cd $DIR/retext-spell && yarn ||true && npm run build || true;
-cd $DIR/retext-syntax-mentions && yarn ||true && npm run build || true;
-cd $DIR/retext-quotes && yarn ||true && npm run build || true;
+cd $DIR/retext-passive && rm -rf .git ||true && rm -rf node_modules ||true && yarn  ||true && npm run build || true;
+cd $DIR/retext-profanities && rm -rf .git ||true && rm -rf node_modules ||true && yarn  ||true && npm run build || true;
+cd $DIR/retext-readability && rm -rf .git ||true && rm -rf node_modules ||true && yarn  ||true && npm run build || true;
+cd $DIR/retext-redundant-acronyms && rm -rf .git ||true && rm -rf node_modules ||true && yarn  ||true && npm run build || true;
+cd $DIR/retext-repeated-words && rm -rf .git ||true && rm -rf node_modules ||true && yarn  ||true && npm run build || true;
+cd $DIR/retext-sentence-spacing && rm -rf .git ||true && rm -rf node_modules ||true && yarn  ||true && npm run build || true;
+cd $DIR/retext-sentiment && rm -rf .git ||true && rm -rf node_modules ||true && yarn  || true && npm run build || true;
+cd $DIR/retext-smartypants && rm -rf .git ||true && rm -rf node_modules ||true && yarn  ||true && npm run build || true;
+cd $DIR/retext-spell && rm -rf .git ||true && rm -rf node_modules ||true && yarn  ||true && npm run build || true;
+cd $DIR/retext-syntax-mentions && rm -rf .git ||true && rm -rf node_modules ||true && yarn  ||true && npm run build || true;
+cd $DIR/retext-quotes && rm -rf .git ||true && rm -rf node_modules ||true && yarn  ||true && npm run build || true;
